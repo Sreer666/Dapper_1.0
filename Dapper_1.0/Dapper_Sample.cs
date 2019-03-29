6b13cc2147afe15c89a1d814cf3b3a07bb1bfa11
Sree Ram <sreer666@gmail.com>
2019-03-29 16:30:37 -0400

    Updated All Required Logic

--- Dapper_1.0/Dapper_Sample.cs
+++ Dapper_1.0/Dapper_Sample.cs
@@ -13,19 +13,155 @@ namespace Dapper_1._0
 {
    public class Dapper_Sample
     {
+        //The Execute() method of the Dapper framework can be used to insert, update, or delete data into a database.
+        //The Query() extension method in Dapper enables you to retrieve data from the database and populate data in your object model
+        //The QueryMultiple() It can execute multiple queries within the same command and map results.
+
         static void Main(string[] args)
         {
            IDbConnection EmployeeModeldb = new SqlConnection(CommonVariables.ConnectionStringsEmployeeModel);
 
             if (EmployeeModeldb.Database == "CodeFirstNewDatabaseSample.BloggingContext")
             {
-                string DepartmentsQuery = "Select * from Departments";
-                var DepartmentsResults = EmployeeModeldb.Query<int>(DepartmentsQuery);
 
-                foreach (var item in DepartmentsResults)
+                Console.WriteLine("Retrieve ALL Departments");
+                string DepartmentsSelectQuery = "Select * from Departments";
+                //By using QueryMultiple()
+                var DepartmentsResults = EmployeeModeldb.QueryMultiple(DepartmentsSelectQuery);
+                var qury = DepartmentsResults.Read<Departments>();
+                foreach (var item in qury)
                 {
-                    Console.WriteLine(item);
+                    Console.WriteLine("ID: \t" + item.ID + "\t" + "NAME: \t" + item.Name + "\t" + "Location: \t" + item.Location);
+                    //Console.WriteLine(qury.ID + "\t" + qury.Name + "\t" + qury.Location);
                 }
+
+                Console.WriteLine("END Retrieve ALL Departments");
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                //By using Query()
+                //var DepartmentsResults = EmployeeModeldb.Query<Departments>(DepartmentsSelectQuery);
+
+                //string DepartmentsIDres = "select ID from Departments";
+                Console.WriteLine("Trying to retrive list of ID");
+                var DepartmentsIDResults = EmployeeModeldb.QueryMultiple("select ID from Departments");
+                var quryIDResults = DepartmentsIDResults.Read<Departments>();
+                foreach (var item in quryIDResults)
+                {
+                    Console.WriteLine("ID: \t" + item.ID);
+                    //Console.WriteLine(qury.ID + "\t" + qury.Name + "\t" + qury.Location);
+                }
+                Console.WriteLine("END to retrive list of ID");
+
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+                Console.WriteLine("Retrieve only one ID");
+                var x = quryIDResults.First();
+                Console.WriteLine("SINGLE ID: \t" + x.ID);
+
+                Console.WriteLine("End OF Retrieve only ID");
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                //select Name from Departments
+
+                Console.WriteLine("Trying to retrive list of NAME");
+                var DepartmentsNameResults = EmployeeModeldb.QueryMultiple("select Name from Departments");
+                var quryNameResults = DepartmentsNameResults.Read<Departments>();
+                foreach (var item in quryNameResults)
+                {
+                    Console.WriteLine("Name: \t" + item.Name);
+                    //Console.WriteLine(qury.ID + "\t" + qury.Name + "\t" + qury.Location);
+                }
+                Console.WriteLine("END to retrive list of Name");
+
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+
+                Console.WriteLine("Retrieve only Name");
+                var name = quryNameResults.First();
+                Console.WriteLine("Name: \t" + name.Name);
+
+                Console.WriteLine("End OF Retrieve only Name");
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                //select* from Departments where ID = 1
+                //select* from Departments where Name = 'IT'
+                //select* from Departments where Location = 'Norfolk'
+
+                Console.WriteLine("Retrieve Departments WITH ID");
+
+                var DepartmentsIDResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where ID = 1");
+                var quryIDResultsS = DepartmentsIDResultsS.Read<Departments>().First();
+
+
+                Console.WriteLine("ID: \t" + quryIDResultsS.ID +"\t" +"Name: \t" + quryIDResultsS.Name+ "\t" + "Location: \t" + quryIDResultsS.Location);
+
+                Console.WriteLine("End Of Retrieve Departments WITH ID");
+
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+                Console.WriteLine("Retrieve Departments WITH Name");
+
+                var DepartmentsNameResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where Name = 'IT'");
+                var quryNameResultsS = DepartmentsNameResultsS.Read<Departments>().First();
+
+
+                Console.WriteLine("ID: \t" + quryNameResultsS.ID + "\t" + "Name: \t" + quryNameResultsS.Name + "\t" + "Location: \t" + quryNameResultsS.Location);
+
+                Console.WriteLine("End Of Retrieve Departments WITH Name");
+
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                Console.WriteLine("Retrieve Departments WITH Location");
+
+                var DepartmentsLocationResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where Location = 'Norfolk'");
+                var quryLocationResultsS = DepartmentsLocationResultsS.Read<Departments>().First();
+
+
+                Console.WriteLine("ID: \t" + quryIDResultsS.ID + "\t" + "Name: \t" + quryIDResultsS.Name + "\t" + "Location: \t" + quryIDResultsS.Location);
+
+                Console.WriteLine("End Of Retrieve Departments WITH Location");
+
+                //select ID from Departments where ID =1
+                //select Name from Departments where Name = 'IT'
+                //select Location from Departments where Location = 'Norfolk'
+                //select ID,Name from Departments where ID =1
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+
+                Console.WriteLine("Retrieve Departments WITH ID and NAME");
+
+                var DepartmentslocatidResultsS = EmployeeModeldb.QueryMultiple("select ID,Name from Departments where ID =1");
+                var quryLocationidResultsS = DepartmentslocatidResultsS.Read<Departments>().First();
+
+
+                Console.WriteLine("ID: \t" + quryLocationidResultsS.ID + "\t" + "Name: \t" + quryLocationidResultsS.Name);
+
+                Console.WriteLine("End Of Retrieve Departments WITH ID and NAME");
+
+
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+                //Select * from Departments where ID in (1, 3)
+                //select top 2 * from Departments
+                //select count(ID) from Departments
+                //select *from Departments order by ID ASC, Name DESC
+                //select ID from Departments order by ID DESC
+
+                Console.WriteLine("Retrieve Selected Departments ");
+
+                var DepartmentslocatidselResultsS = EmployeeModeldb.QueryMultiple("Select * from Departments where ID in (1, 3)");
+                var quryLocationidselResultsS = DepartmentslocatidselResultsS.Read<Departments>();
+
+                foreach(var item in quryLocationidselResultsS)
+                {
+                    Console.WriteLine("ID: \t" + item.ID + "\t" + "NAME: \t" + item.Name + "\t" + "Location: \t" + item.Location);
+                }
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
             }
         }
     }
---
+++ Dapper_1.0/Departments.cs
@@ -0,0 +1,10 @@
+﻿namespace Dapper_1._0
+{
+    internal class Departments
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string  Location { get; set; }
+    }
+}
