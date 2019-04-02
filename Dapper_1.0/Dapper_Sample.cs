6037aca789d628196139e9d938dedd0386a53d0a
Sree Ram Git <sreer666@gmail.com>
2019-04-02 10:09:40 -0400

    code Refactor

--- Dapper_1.0/Dapper_Sample.cs
+++ Dapper_1.0/Dapper_Sample.cs
@@ -24,6 +24,7 @@ namespace Dapper_1._0
             if (EmployeeModeldb.Database == "CodeFirstNewDatabaseSample.BloggingContext")
             {
 
+                #region Retrieve ALL Departments
                 Console.WriteLine("Retrieve ALL Departments");
                 string DepartmentsSelectQuery = "Select * from Departments";
                 //By using QueryMultiple()
@@ -36,8 +37,11 @@ namespace Dapper_1._0
                 }
 
                 Console.WriteLine("END Retrieve ALL Departments");
+                #endregion
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+                #region Trying to retrive list of ID
                 //By using Query()
                 //var DepartmentsResults = EmployeeModeldb.Query<Departments>(DepartmentsSelectQuery);
 
@@ -52,9 +56,10 @@ namespace Dapper_1._0
                 }
                 Console.WriteLine("END to retrive list of ID");
 
-
+                #endregion
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
 
+                #region Retrieve only one ID
                 Console.WriteLine("Retrieve only one ID");
                 var x = quryIDResults.First();
                 Console.WriteLine("SINGLE ID: \t" + x.ID);
@@ -62,8 +67,11 @@ namespace Dapper_1._0
                 Console.WriteLine("End OF Retrieve only ID");
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                #endregion
+
                 //select Name from Departments
 
+                #region Trying to retrive list of NAME
                 Console.WriteLine("Trying to retrive list of NAME");
                 var DepartmentsNameResults = EmployeeModeldb.QueryMultiple("select Name from Departments");
                 var quryNameResults = DepartmentsNameResults.Read<Departments>();
@@ -72,36 +80,42 @@ namespace Dapper_1._0
                     Console.WriteLine("Name: \t" + item.Name);
                     //Console.WriteLine(qury.ID + "\t" + qury.Name + "\t" + qury.Location);
                 }
-                Console.WriteLine("END to retrive list of Name");
+                Console.WriteLine("END to retrive list of Name"); 
+                #endregion
 
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
 
+                #region Retrieve only Name
 
                 Console.WriteLine("Retrieve only Name");
                 var name = quryNameResults.First();
                 Console.WriteLine("Name: \t" + name.Name);
 
-                Console.WriteLine("End OF Retrieve only Name");
+                Console.WriteLine("End OF Retrieve only Name"); 
+                #endregion
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
                 //select* from Departments where ID = 1
                 //select* from Departments where Name = 'IT'
                 //select* from Departments where Location = 'Norfolk'
 
+                #region Departments WITH ID
                 Console.WriteLine("Retrieve Departments WITH ID");
 
                 var DepartmentsIDResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where ID = 1");
                 var quryIDResultsS = DepartmentsIDResultsS.Read<Departments>().First();
 
 
-                Console.WriteLine("ID: \t" + quryIDResultsS.ID +"\t" +"Name: \t" + quryIDResultsS.Name+ "\t" + "Location: \t" + quryIDResultsS.Location);
+                Console.WriteLine("ID: \t" + quryIDResultsS.ID + "\t" + "Name: \t" + quryIDResultsS.Name + "\t" + "Location: \t" + quryIDResultsS.Location);
 
-                Console.WriteLine("End Of Retrieve Departments WITH ID");
+                Console.WriteLine("End Of Retrieve Departments WITH ID"); 
+                #endregion
 
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
 
+                #region Retrieve Departments WITH Name
                 Console.WriteLine("Retrieve Departments WITH Name");
 
                 var DepartmentsNameResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where Name = 'IT'");
@@ -110,10 +124,12 @@ namespace Dapper_1._0
 
                 Console.WriteLine("ID: \t" + quryNameResultsS.ID + "\t" + "Name: \t" + quryNameResultsS.Name + "\t" + "Location: \t" + quryNameResultsS.Location);
 
-                Console.WriteLine("End Of Retrieve Departments WITH Name");
+                Console.WriteLine("End Of Retrieve Departments WITH Name"); 
+                #endregion
 
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                #region Retrieve Departments WITH Location
                 Console.WriteLine("Retrieve Departments WITH Location");
 
                 var DepartmentsLocationResultsS = EmployeeModeldb.QueryMultiple("select* from Departments where Location = 'Norfolk'");
@@ -122,7 +138,8 @@ namespace Dapper_1._0
 
                 Console.WriteLine("ID: \t" + quryIDResultsS.ID + "\t" + "Name: \t" + quryIDResultsS.Name + "\t" + "Location: \t" + quryIDResultsS.Location);
 
-                Console.WriteLine("End Of Retrieve Departments WITH Location");
+                Console.WriteLine("End Of Retrieve Departments WITH Location"); 
+                #endregion
 
                 //select ID from Departments where ID =1
                 //select Name from Departments where Name = 'IT'
@@ -132,6 +149,7 @@ namespace Dapper_1._0
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
 
 
+                #region Retrieve Departments WITH ID and NAME
                 Console.WriteLine("Retrieve Departments WITH ID and NAME");
 
                 var DepartmentslocatidResultsS = EmployeeModeldb.QueryMultiple("select ID,Name from Departments where ID =1");
@@ -144,6 +162,7 @@ namespace Dapper_1._0
 
 
                 Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                #endregion
 
                 //Select * from Departments where ID in (1, 3)
                 //select top 2 * from Departments
@@ -151,16 +170,18 @@ namespace Dapper_1._0
                 //select *from Departments order by ID ASC, Name DESC
                 //select ID from Departments order by ID DESC
 
+                #region Retrieve Selected Departments
                 Console.WriteLine("Retrieve Selected Departments ");
 
                 var DepartmentslocatidselResultsS = EmployeeModeldb.QueryMultiple("Select * from Departments where ID in (1, 3)");
                 var quryLocationidselResultsS = DepartmentslocatidselResultsS.Read<Departments>();
 
-                foreach(var item in quryLocationidselResultsS)
+                foreach (var item in quryLocationidselResultsS)
                 {
                     Console.WriteLine("ID: \t" + item.ID + "\t" + "NAME: \t" + item.Name + "\t" + "Location: \t" + item.Location);
                 }
-                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); 
+                #endregion
 
             }
         }
@@ -170,6 +191,9 @@ namespace Dapper_1._0
         public static String ConnectionStringsEmployeeModel
         {
             get { return ConfigurationManager.ConnectionStrings["EmployeeModel"].ConnectionString; }
+        }    public static String ConnectionStringsMedisys_Dev
+        {
+            get { return ConfigurationManager.ConnectionStrings["Medisys_Dev"].ConnectionString; }
         }
     }
 }
